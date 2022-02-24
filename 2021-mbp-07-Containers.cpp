3ffb9ba363f1f787b4c5108ae6d6abce2933f437
kvbolashikova18 <kvbolashikova18@codingburgas.bg>
2022-02-24 09:20:17 +0200

    Implement method union

--- 2021-mbp-07-Containers.cpp
+++ 2021-mbp-07-Containers.cpp
@@ -1,20 +1,44 @@
-// 2021-mbp-07-Containers.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <set>
+#include <iterator>
+#include <cstring>
+
+using namespace std;
+
+set<string>::iterator it;
+
+bool contains(set<string> setToCheck, string valueToCheck) {
+	for (it = setToCheck.begin(); it != setToCheck.end(); ++it) {
+		if (*it == valueToCheck) {
+			return true;
+		}
+	}
+	return true;
+}
+
+void unionWith(set<string> setA, set<string> setB) {
+	set<string> setTemp;
+
+	for (it = setA.begin(); it != setA.end(); ++it) {
+		setTemp.insert(*it);
+	}
+
+	for (it = setB.begin(); it != setB.end(); ++it) {
+		if (!contains(setTemp, *it)) {
+			setTemp.insert(*it);
+		}
+	}
+
+	for (it = setTemp.begin(); it != setTemp.end(); ++it) {
+		cout << *it << " / ";
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	set<string> setA = { "Ivan", "Kaloyan", "Kiril", "Mario" };
+
+	set<string> setB = { "Martin", "Mario", "Petar"," Kiril" };
+
+	unionWith(setA, setB);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
